ab13006d5ef2678df065257fdb05ff3b0ed59813
mcdocdao <macdocdao@gmail.com>
2024-06-28 17:34:46 +0700

    update portfolio website

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react';
 import flag from '../media/thai-flag.png';
 import downLoadIcon from '../media/download.png';
 import '../style/AboutMe.css';
-import resume from '../media/noppawatresumepdf2.pdf';
+import resume from '../media/NoppawatChotiwanresume2024.pdf';
 import githubIcon from '../media/github-logo.png';
 import reading from '../media/reading.png';
 const AboutMe=()=>{
@@ -11,19 +11,21 @@ const AboutMe=()=>{
     const resumeFile = resume;
     const gitIcon = githubIcon;
     const readingIcon = reading;
+    
     return(
         <div className='container mt-5' id='aboutme'>
             <div className='row justify-content-center'>
                 <div className='col-8 col-xs-12 jumbotron rounded border-bottom border-right boxSha bg-light pt-4 pb-4'>
                     <h1 className="display-6 text-center">About me</h1>
-                    <p className="lead mx-3 text-center">I'm <span className='hvr-underline-from-center'>Noppawat Chotiwan</span>, a 23 years old self-taught developer <span><img  className ='bounce'src={readingIcon} style={{width:'1rem'}}/></span> from Thailand <span><img className='bounce'src={thaiFlag} style={{width:'1.5rem'}}alt='Thai flag'/></span><br/>
-                    I worked as a Quality Assurance Engineer at <span className='font-italic'><a href='https://www.wongnai.com/' target='_blank'>Wongnai Media </a></span>
-                    from Febuary to August.  My job was to ensure the quality of the software by manual test and automating using <span className='font-italic'><a href='https://pptr.dev/' target='_blank'>Puppeteer  </a></span> 
-                    After having been worked for some months, I realized that I wanted to learn more on web development and 
-                    become a front end developer.  Then I decided to start learning html, css, 
-                    javascript and react. After a while I'm so into the react and decided for 
-                    taking change.  In August I decided to leave Wongnai Media to pursue on my 
-                    passion to become a front end developer.
+                    <p className="lead mx-3 text-center">I'm <span className='hvr-underline-from-center'>Noppawat Chotiwan</span>, a 28 years old self-taught developer <span><img  className ='bounce'src={readingIcon} style={{width:'1rem'}}/></span> from Thailand <span><img className='bounce'src={thaiFlag} style={{width:'1.5rem'}}alt='Thai flag'/></span><br/>
+                    With a robust background in developing and maintaining web applications,
+                    <br/> I'm currently work at <span className='font-italic'><a href='https://fptsoftware.com/' target='_blank'>FPT Software</a></span>, focusing on high-quality web applications using React, Next.js, and TypeScript. 
+                    <br/>
+                    At <span className='font-italic'><a href='https://www.moneyherogroup.com/' target='_blank'>MoneyHero Group</a></span>, I boosted conversion rates and enhanced UI/UX for platforms like Money101 and Singsaver.
+                    <br/>
+                    Previously, I developed automation tests at <span className='font-italic'><a href='https://www.wongnai.com/' target='_blank'>Wongnai</a></span> and led end-to-end testing projects at <span className='font-italic'><a href='https://pomelofashion.com/th/th/' target='_blank'>Pomelo</a></span>. Recognized with several awards.
+                    <br/>
+                    With a passion for frontend development, fintech, OTA and startups, I continuously strive to innovate and deliver exceptional results.
                     <br/>If you want to know more about me, please click resume button below.   Thanks!.
                     
                     </p>
--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,111 +1,185 @@
-import React from 'react';
-import nytgif from '../media/nytreackhookgif.gif';
-import youtubeSearchGif from '../media/youtube-api-react.gif';
-import vending from '../media/vendingmachine.gif';
-import startup from '../media/startup.png';
-import githubIcon from '../media/github-logo.png';
-import dogListGif from '../media/react-router-dog-list.gif';
-import todoredux from '../media/todolistreduxreduxform.gif';
-import pokedexGif from '../media/pokedex.gif';
-import '../style/Portfolio.css';
-import restaurantmanager from '../media/restaurant-manager.gif';
-const Portfolio=()=>{
-    const nytGif = nytgif;
-    const youtubeGif = youtubeSearchGif;
-    const startupIcon = startup;
-    const gitIcon = githubIcon;
-    const dogList = dogListGif;
-    const vendingGif = vending;
-    const todoReduxGif = todoredux;
-    const restgif = restaurantmanager;
-    const pokedex = pokedexGif;
-    const listItems =[{
-                        imgSrc:nytGif,
-                        title:'BANGKOK TIME',
-                        detail:'News App, where the users can pick the news by month and year. The app uses NewYork Time API for the resources.',
-                        stack:'REACT-REACTHOOK-BOOTSTRAP-AXIOS',
-                        pageSrc:'https://nytreacthookpractice.firebaseapp.com/',
-                        gitHubSrc:'https://github.com/mcguyvor/react-hook-new-york-time-api'
-                        },
-                        {
-                        imgSrc:youtubeGif,
-                        title:'YOUTUBE API SEARCH',
-                        detail:'Youtube App for the users to search their video.',
-                        stack:'REACT-AXIOS',
-                        pageSrc:'https://api-react-350b3.firebaseapp.com/',
-                        gitHubSrc:'https://github.com/mcguyvor/react-search-with-youtube-api'
-                        },
-                        {
-                        imgSrc:dogList,
-                        title:'Dog List with react-router',
-                        detail: "Dog list App for dog lovers to the dogs' profile",
-                        stack:'REACT-REACT ROUTER',
-                        pageSrc:'https://react-router-dog-list.firebaseapp.com',
-                        gitHubSrc:'https://github.com/mcguyvor/doglist-for-react-router-practice-with-navlink-nav'
-                        },
-                        {
-                        imgSrc:vendingGif,
-                        title: 'Vending machine',
-                        detail:'Vending machine app provided three differents items for the user to pick',
-                        stack:'REACT-REACT ROUTER-REDUX',
-                        pageSrc:'https://vendingmachinereactrouterredux.firebaseapp.com',
-                        gitHubSrc:'https://github.com/mcguyvor/vending-machine-react-routing-redux-prac'
-                        },
-                        {
-                        imgSrc:todoReduxGif,
-                        title:'Todo List with redux form',
-                        detail:"Todo List app for the users to add their To do's daily. They can filter their To do's by clicking the filter button to show all to do's or completed to do's.",
-                        stack:'REACT-REDUX-REDUX_FORM_BOOTSTRAP',
-                        pageSrc:'https://todolist-react-redux-reduxform.firebaseapp.com/',
-                        gitHubSrc:'https://github.com/mcguyvor/todolist-redux/tree/master/src'
-                        },
-                        {
-                        imgSrc:restgif,
-                        title:'Restaurant manager',
-                        detail:'Restaurant manager web app, enable users to add or remove products and order products from the website.',
-                        stack:'REACT-REDUX-REDUX-FORM-BOOTSTRAP-JSON SERVER-AXIOS',
-                        pageSrc:null,
-                        gitHubSrc:'https://github.com/mcguyvor/client'
-                        },
-                        {
-                        imgSrc:pokedex,
-                        title:'Pokedex',
-                        detail:'My very first React web app, this pokedex game calculates the scores for each player by its experience',
-                        stack:'REACT',
-                        pageSrc:'https://pokedex-473d5.firebaseapp.com/',
-                        gitHubSrc:'https://github.com/mcguyvor/pokedex'
-                            }
-];
-    return( 
-        <div className='container ' id='portfolio'>
-            <h1 className='title text-center'>Portfolio</h1>
-            <div className='card-columns'>
-                {listItems.map((idx=>{return(
-                <div className='card p-2 sha threed col-xs-12'>
-                    <img style={{width:'100%'}}src={idx.imgSrc} className='shadow-lg rounded card-img-top p-2'/> 
-                        <div className="card-body">
-                            <h5 className="card-title title font-weight-bold text-monospace"><u>{idx.title}</u></h5>
-                            <p className="card-text">{idx.detail}</p>
-                            <p className="card-text font-weight-bold text-center"><small><strong>{idx.stack}</strong></small></p>
-                            <div className='d-flex justify-content-center'>
-                                {idx.pageSrc!==null?
-                                <a className="btn btn-primary rounded-pill mr-2 shadow btn-change" href={idx.pageSrc} role="button" target='_blank'>
-                                <span><img src={startupIcon} className='mr-1'style={{width:'1rem'}}/></span>
-                                Visit</a>
-                                : null}
-                                <a className="btn btn-outline-primary rounded-pill shadow btn-change" href={idx.gitHubSrc} role="button" target='_blank'>
-                                <span><img src={gitIcon} className='mr-1' style={{width:'1rem'}}/></span>
-                                Github</a>
-                            </div>
-                        </div>
+import React from "react";
+import nytgif from "../media/nytreackhookgif.gif";
+import youtubeSearchGif from "../media/youtube-api-react.gif";
+import vending from "../media/vendingmachine.gif";
+import startup from "../media/startup.png";
+import githubIcon from "../media/github-logo.png";
+import dogListGif from "../media/react-router-dog-list.gif";
+import todoredux from "../media/todolistreduxreduxform.gif";
+import pokedexGif from "../media/pokedex.gif";
+import ecommerse from "../media/ecommerse.png";
+import todolist from "../media/todolist.gif";
+import moviedb from "../media/moviedb.gif";
+
+import "../style/Portfolio.css";
+import restaurantmanager from "../media/restaurant-manager.gif";
+const Portfolio = () => {
+  const nytGif = nytgif;
+  const youtubeGif = youtubeSearchGif;
+  const startupIcon = startup;
+  const gitIcon = githubIcon;
+  const dogList = dogListGif;
+  const vendingGif = vending;
+  const todoReduxGif = todoredux;
+  const restgif = restaurantmanager;
+  const pokedex = pokedexGif;
+  const ecommerseImg = ecommerse;
+  const todo = todolist;
+  const movie = moviedb;
+  const listItems = [
+    {
+      imgSrc: movie,
+      title: "Movie database",
+      detail: "Movie Database landing page",
+      stack: "REACT-REACTHOOK-BOOTSTRAP-AXIOS-REACTROUTERDOM",
+      pageSrc: "https://moviemacboss.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/moviedbmacboss",
+    },
+    {
+      imgSrc: todo,
+      title: "Todo List with redux form",
+      detail:
+        "Todo List app for the users to add their To do's daily. They can filter their To do's by clicking the filter button to show all to do's or completed to do's.",
+      stack: "REACT-REDUX-REDUX_FORM_BOOTSTRAP",
+      pageSrc: "https://todolist-react-redux-reduxform.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/todolist-redux/tree/master/src",
+    },
+    {
+      imgSrc: ecommerseImg,
+      title: "E commerse webstyle",
+      detail:
+        "E commerse webstyle with google signup. Primary working on design pattern and optimization .Currently working",
+      stack: "REACT-REACTHOOK-REACTROUTER-REDUX-FIREBASE-GOOGLEAUTH-RESELECT",
+      pageSrc: "https://crown-db-a4e72.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/Miniblog-project",
+    },
+    ,
+    {
+      imgSrc: nytGif,
+      title: "BANGKOK TIME",
+      detail:
+        "News App, where the users can pick the news by month and year. The app uses NewYork Time API for the resources.",
+      stack: "REACT-REACTHOOK-BOOTSTRAP-AXIOS",
+      pageSrc: "https://nytreacthookpractice.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/react-hook-new-york-time-api",
+    },
+    {
+      imgSrc: youtubeGif,
+      title: "YOUTUBE API SEARCH",
+      detail: "Youtube App for the users to search their video.",
+      stack: "REACT-AXIOS",
+      pageSrc: "https://api-react-350b3.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/react-search-with-youtube-api",
+    },
+    {
+      imgSrc: dogList,
+      title: "Dog List with react-router",
+      detail: "Dog list App for dog lovers to the dogs' profile",
+      stack: "REACT-REACT ROUTER",
+      pageSrc: "https://react-router-dog-list.firebaseapp.com",
+      gitHubSrc:
+        "https://github.com/mcguyvor/doglist-for-react-router-practice-with-navlink-nav",
+    },
+    {
+      imgSrc: vendingGif,
+      title: "Vending machine",
+      detail:
+        "Vending machine app provided three differents items for the user to pick",
+      stack: "REACT-REACT ROUTER-REDUX",
+      pageSrc: "https://vendingmachinereactrouterredux.firebaseapp.com",
+      gitHubSrc:
+        "https://github.com/mcguyvor/vending-machine-react-routing-redux-prac",
+    },
+    {
+      imgSrc: todoReduxGif,
+      title: "Todo List with redux form",
+      detail:
+        "Todo List app for the users to add their To do's daily. They can filter their To do's by clicking the filter button to show all to do's or completed to do's.",
+      stack: "REACT-REDUX-REDUX_FORM_BOOTSTRAP",
+      pageSrc: "https://todolist-react-redux-reduxform.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/todolist-redux/tree/master/src",
+    },
+    {
+      imgSrc: restgif,
+      title: "Restaurant manager",
+      detail:
+        "Restaurant manager web app, enable users to add or remove products and order products from the website.",
+      stack: "REACT-REDUX-REDUX-FORM-BOOTSTRAP-JSON SERVER-AXIOS",
+      pageSrc: null,
+      gitHubSrc: "https://github.com/mcguyvor/client",
+    },
+    {
+      imgSrc: pokedex,
+      title: "Pokedex",
+      detail:
+        "My very first React web app, this pokedex game calculates the scores for each player by its experience",
+      stack: "REACT",
+      pageSrc: "https://pokedex-473d5.firebaseapp.com/",
+      gitHubSrc: "https://github.com/mcguyvor/pokedex",
+    },
+  ];
+  return (
+    <div className="container " id="portfolio">
+      <h1 className="title text-center">Portfolio</h1>
+      <div className="card-columns">
+        {listItems.map((idx) => {
+          return (
+            <div className="card p-2 sha threed col-xs-12">
+              <img
+                style={{ width: "100%" }}
+                src={idx.imgSrc}
+                className="shadow-lg rounded card-img-top p-2"
+              />
+              <div className="card-body">
+                <h5 className="card-title title font-weight-bold text-monospace">
+                  <u>{idx.title}</u>
+                </h5>
+                <p className="card-text">{idx.detail}</p>
+                <p className="card-text font-weight-bold text-center">
+                  <small>
+                    <strong>{idx.stack}</strong>
+                  </small>
+                </p>
+                <div className="d-flex justify-content-center">
+                  {idx.pageSrc !== null ? (
+                    <a
+                      className="btn btn-primary rounded-pill mr-2 shadow btn-change"
+                      href={idx.pageSrc}
+                      role="button"
+                      target="_blank"
+                    >
+                      <span>
+                        <img
+                          src={startupIcon}
+                          className="mr-1"
+                          style={{ width: "1rem" }}
+                        />
+                      </span>
+                      Visit
+                    </a>
+                  ) : null}
+                  <a
+                    className="btn btn-outline-primary rounded-pill shadow btn-change"
+                    href={idx.gitHubSrc}
+                    role="button"
+                    target="_blank"
+                  >
+                    <span>
+                      <img
+                        src={gitIcon}
+                        className="mr-1"
+                        style={{ width: "1rem" }}
+                      />
+                    </span>
+                    Github
+                  </a>
                 </div>
-                )})
-                )
-                }
-                
+              </div>
             </div>
-        </div>
-        
-    );
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default Portfolio;
